86149126eace3b1cfa0a79a3ab33fee8be3387a2
it63070160 <71114745+it63070160@users.noreply.github.com>
2022-12-13 03:12:39 +0700

    Update script.js
    add a carousel event listener and text function for the future

--- js/script.js
+++ js/script.js
@@ -1,32 +1,41 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+var listText = {
+  "myHTMLCarousel-inner": {
+    "detail" : [
+      {'slideID': "Solar", 'name' : "Solar Rooftop Landing Page", 'description':"โปรเจกต์วิชา Multimedia Technology ทำร่วมกับ Greenpeace Thailand"}
+    ]
+  },
+  "myRANSCarousel-inner": {
+    "detail" : [
+      {'slideID': "RANS", 'name':"Road Risk Areas Notification System",'description':"แอปพลิเคชันทางโทรศัพท์สำหรับแจ้งเตือนจุดเสี่ยงบนท้องถนนเมื่อผู้ใช้เข้าใกล้จุดเสี่ยง"}
+    ]
+  }
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function nextText(info){
+  // var element = document.querySelector('#'+info.id+' > div.carousel-item.active');
+  // var elementAll = document.querySelectorAll('#'+info.id+' .carousel-item').length;
+  var detailFilter = listText[info.parentID].detail.filter((value)=>{
+    return value.slideID == info.slideID
+  })
+  document.querySelector('#'+info.parentID+'_expName').innerHTML = detailFilter[0].name
+  document.querySelector('#'+info.parentID+'_expDesc').innerHTML = detailFilter[0].description
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  console.log(slides.length)
-  if(slides.length==1){
-    dots[0].style.display = "none";
-  }
+function prevText(info){
+  var detailFilter = listText[info.parentID].detail.filter((value)=>{
+    return value.slideID == info.slideID
+  })
+  document.querySelector('#'+info.parentID+'_expName').innerHTML = detailFilter[0].name
+  document.querySelector('#'+info.parentID+'_expDesc').innerHTML = detailFilter[0].description
+}
+
+const myCarousel = document.getElementsByClassName('carousel')
+for(var i=0; i<myCarousel.length; i++){
+  myCarousel[i].addEventListener('slide.bs.carousel', event => {
+    if(event.direct == 'right'){
+      prevText({'parentID':event.relatedTarget.parentElement.id, 'slideID':event.relatedTarget.id})
+    }else{
+      nextText({'parentID':event.relatedTarget.parentElement.id, 'slideID':event.relatedTarget.id})
+    }
+  })
 }
